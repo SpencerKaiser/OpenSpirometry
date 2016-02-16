8f2e5d730fa129d8c6cd3468bddbaa952448d199
Spencer Kaiser <skaiser@me.com>
2016-02-15 18:04:57 -0600

    Removing old code

--- OpenSpirometry/ModalActionPageViewController.m
+++ OpenSpirometry/ModalActionPageViewController.m
@@ -32,36 +32,7 @@
     self.actionLabel.text = self.pageConfig[@"Label"];
     self.actionDescription.text = self.pageConfig[@"Description"];
     [self.actionButton setTitle:self.pageConfig[@"Button"] forState:UIControlStateNormal];
-    
-//    self.actionButton.enabled = false;
-//    
-//    self.timeRemaining = timerDuration;
-//    self.actionTimerLabel.text = [NSString stringWithFormat:@"%.0f", self.timeRemaining];
-//    
-//    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer:) userInfo:nil repeats:YES];
-//    
-//    UITapGestureRecognizer *tripleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tripleTapHandler:)];
-//    tripleTap.numberOfTapsRequired = 3;
-//    [self.view addGestureRecognizer:tripleTap];
 }
-
-//- (void)tripleTapHandler:(UIGestureRecognizer *)gestureRecognizer {
-//    [self.timer invalidate];
-//    self.actionTimerLabel.text = @"";
-//    self.actionButton.enabled = true;
-//}
-
-//- (void)updateTimer:(NSTimer*)timer {
-//    if (self.timeRemaining >= 1.0) {
-//        self.timeRemaining -= 1;
-//        self.actionTimerLabel.text = [NSString stringWithFormat:@"%.0f", self.timeRemaining];
-//    } else {
-//        [timer invalidate];
-//        self.actionTimerLabel.text = @"";
-//        self.actionButton.enabled = true;
-//    }
-//
-//}
 
 - (IBAction)actionButtonPressed:(id)sender {
     if([self.actionPageDelegate respondsToSelector:@selector(userActionTaken)])
