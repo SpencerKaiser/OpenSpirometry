3769051d8a732cc17280b2b05339bc23db258622
Spencer Kaiser <skaiser@me.com>
2016-02-25 17:38:33 -0600

    Moving all persistent storage functionality to a new object (UserDataHandler)

--- OpenSpirometry/SpirometerTestAnalyzer.m
+++ OpenSpirometry/SpirometerTestAnalyzer.m
@@ -7,10 +7,11 @@
 //
 
 #import "SpirometerTestAnalyzer.h"
+#import "UserDataHandler.h"
 #import <Foundation/Foundation.h>
 
-#define numEffortsForFixedDurationType 4
-#define TimeStamp [NSString stringWithFormat:@"%f",[[NSDate date] timeIntervalSince1970] * 1000]
+#define numEffortsForFixedDurationType 1
+#define TimeStamp [NSString stringWithFormat:@"%f",[[NSDate date] timeIntervalSince1970]]
 
 @interface SpirometerTestAnalyzer()
 @property (nonatomic, assign) SpiroTestState currentState;
@@ -20,7 +21,9 @@
 @property (strong, nonatomic) NSString* userIdentifier;
 @property (strong, nonatomic) NSMutableDictionary* userDataFile;        // Pointer to all user data
 @property (strong, nonatomic) NSMutableDictionary* testData;            // Pointer to current test data
-@property (strong, nonatomic) NSString* userDataFilePath;               // Path to user data file
+//@property (strong, nonatomic) NSString* userDataFilePath;               // Path to user data file
+
+@property (strong, nonatomic) UserDataHandler* userDataHandler;
 @end
 
 
@@ -38,7 +41,7 @@
     return _sharedInstance;
 }
 
--(id)init {
+- (id)init {
     if(self = [super init]){
         [self setup];
         return self;
@@ -46,26 +49,27 @@
     return nil;
 }
 
--(void)dealloc {
+- (void)dealloc {
     
 }
 
--(void)setup {
+- (void)setup {
     //Perform any actions (instatiating variables, etc.) that must be done upon object creation
     //This will be done within the init() function, prior to returning a copy of this object to its calling function
     [self createTestDataContainer];
-    self.currentState = SpiroTestStateNoEffortsAdded;
     
     self.testType = SpiroTestTypeStandard;  //Default test type is set to standard
+    
+    self.userDataHandler = [[UserDataHandler alloc] init];
 }
 
 
 #pragma mark - PUBLIC INTERFACE
--(SpiroTestState)addEffortResults:(NSDictionary*)effortResults {
+- (SpiroTestState)addEffortResults:(NSDictionary*)effortResults {
     // Add the new effort to the efforts array within the current test
     [self.testData[@"Efforts"] addObject:effortResults];
     
-    [self evaluateEfforts];
+    [self evaluateEfforts]; // Check test status and write data to memory
     
     return self.currentState;
 }
@@ -74,22 +78,26 @@
     [self.testData[@"Efforts"] removeLastObject];
     return [self addEffortResults:effortResults];
 }
-
+ 
 - (void)addTestNotes:(NSString*)notes {
     if (self.testData[@"Notes"]) {
         notes = [NSString stringWithFormat:@"[%@] Appended: %@", self.testData[@"Notes"], notes];
     }
     self.testData[@"Notes"] = notes;
-    [self writeUserDataToMemory];
+    
+    [self.userDataHandler writeUserDataToMemory:self.userDataFile];
 }
 
 -(SpiroTestState)getCurrentSpiroTestState {
     return self.currentState;
 }
 
--(void)clearAllEfforts {
-    //Call function to reinstatiate data container
+- (void)clearAllEfforts {
+    //Call function to reinstantiate data container
     [self createTestDataContainer];
+    
+    // !!!: NOT TESTED
+    // This function only clears the efforts from local memory but does not clear what has already been written to persistent storage
 }
 
 -(void)setSpiroTestType:(SpiroTestType)testType{
@@ -98,7 +106,7 @@
 
 
 #pragma mark - PRIVATE INTERFACE
--(void)evaluateEfforts {
+- (void)evaluateEfforts {
     switch (self.testType) {
         case SpiroTestTypeStandard:
             [self evaluateForStandardType];
@@ -108,30 +116,24 @@
             break;
     }
     
-    // TODO: If test complete, write timestamp to self.testData
+    // Check if test is complete... if complete, add timestamps to test data
+    // Entering this function will result in data being written to memory
     if (self.currentState == SpiroTestStateTestComplete) {
-        [self addFieldsToTestData:[[NSDictionary alloc] initWithObjectsAndKeys:TimeStamp, @"Completion", nil] ];
-        
-        NSDate *myDate = [[NSDate alloc] init];
         NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
         [dateFormat setDateFormat:@"cccc, MMMM dd, yyyy, hh:mm aa"];
-        NSString *prettyTimeStamp = [dateFormat stringFromDate:myDate];
+        NSString *prettyTimeStamp = [dateFormat stringFromDate:[[NSDate alloc] init]];
         
-        // TODO: Use timestamp to continue test after a crash
-        
-        [self addFieldsToTestData:[[NSDictionary alloc] initWithObjectsAndKeys:TimeStamp, @"Completion", prettyTimeStamp, @"Completion (Formatted)", nil]];
-        
-        [self addMetadataToUserData:[[NSDictionary alloc] initWithObjectsAndKeys:self.userIdentifier, @"User ID", nil]];
+        [self addFieldsToTestData:[[NSDictionary alloc] initWithObjectsAndKeys:TimeStamp, @"Completion", prettyTimeStamp, @"CompletionFormatted", nil]];
+    } else {
+        // If this block is reached, save to memory as it has not yet been saved
+        [self.userDataHandler writeUserDataToMemory:self.userDataFile];
     }
-    
-    
-    [self writeUserDataToMemory];
 }
 
--(void)evaluateForStandardType {
+- (void)evaluateForStandardType {
     // TODO: Test Analysis Logic
     // Need to evaluate the effort data and determine whether the test should conclude
-    if (self.testData.count >= 4 || self.testData.count >= 5) {
+    if (self.testData.count >= numEffortsForFixedDurationType) {
         self.currentState = SpiroTestStateTestComplete;
     }
     else {
@@ -139,7 +141,7 @@
     }
 }
 
--(void)evaluateForFixedDurationType {
+- (void)evaluateForFixedDurationType {
     // Grab reference to current efforts for test (including recently added effort)
     NSMutableArray* efforts = [self.testData objectForKey:@"Efforts"];
     
@@ -153,7 +155,7 @@
 }
 
 
--(void)createTestDataContainer {
+- (void)createTestDataContainer {
     self.testData = [[NSMutableDictionary alloc] init];
     
     //Create empty array to contain efforts and push it into the test data dictionary
@@ -162,84 +164,32 @@
     self.currentState = SpiroTestStateNoEffortsAdded;
 }
 
--(void)addUserIdentifier:(NSString *)userIdentifier {
+- (void)addUserIdentifier:(NSString *)userIdentifier {
     self.userIdentifier = userIdentifier;
+    self.userDataHandler.userID = self.userIdentifier;
 }
 
 #pragma mark - USER DATA STORAGE
--(void)addFieldsToTestData: (NSDictionary*) additionalFields {
-     [self.testData addEntriesFromDictionary:additionalFields];
-    
-    // Save test data to memory
-    [self writeUserDataToMemory];
+- (void)addFieldsToTestData: (NSDictionary*) additionalFields {
+    [self checkStorageParams];
+    [self.testData addEntriesFromDictionary:additionalFields];
+    [self.userDataHandler writeUserDataToMemory:self.userDataFile];
 }
 
--(void)addMetadataToUserData: (NSDictionary*) additionalFields {
-    // Metadata consists of data added to the root level of the User Data file (e.g., UserID)
+- (void)addMetadataToUserData: (NSDictionary*) additionalFields {
+    [self checkStorageParams];    
+    [self.userDataHandler writeMetadata:additionalFields toUserData:self.userDataFile];
+}
+
+- (void)checkStorageParams {
     if (!self.userDataFile) {
-        [self getUserDataDocument];
+        if (!self.userIdentifier) {
+            [NSException raise:@"No User ID Found" format:@"User ID is required for this type of interaction with the UserDataHandler"];
+        }
+        self.userDataFile = [self.userDataHandler getUserDataForID:self.userIdentifier];
+        NSMutableArray* tests = self.userDataFile[@"Tests"];
+        [tests addObject:self.testData];
     }
-    
-    if (!self.userDataFile[@"Metadata"]) {
-        // If metadata dictionary doesn't exist, create it
-        self.userDataFile[@"Metadata"] = [[NSMutableDictionary alloc] init];
-    }
-    
-    [self.userDataFile[@"Metadata"] addEntriesFromDictionary:additionalFields];
-    
-    // Save metadata to memory
-    [self writeUserDataToMemory];
-}
-
--(void)getUserDataDocument {
-    // Get UI Documents Paths
-    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    // Get path to documents root
-    NSString* documents = [paths firstObject];
-    
-    // If no user ID is supplied, assume a single user for the device; store data into "UserData.json"
-    if (!self.userIdentifier) {
-        self.userIdentifier = @"UserData";
-    }
-    // Set local path variable
-    self.userDataFilePath = [NSString stringWithFormat:@"%@/%@.json", documents, self.userIdentifier];
-    NSLog(@"File Path: %@", self.userDataFilePath);
-    
-    if ([[NSFileManager defaultManager] fileExistsAtPath:self.userDataFilePath]) {
-        // File for user exists...
-        // Read raw data into NSData object
-        NSData* rawUserData = [[NSData alloc] initWithContentsOfFile:self.userDataFilePath];
-        self.userDataFile = [self objectFromJSON:rawUserData];
-        [self.userDataFile[@"Tests"] addObject:self.testData];
-    } else {
-        // File for user does not exist...
-        // Create mutable array to contain all user file data
-        self.userDataFile = [[NSMutableDictionary alloc] init];
-        self.userDataFile[@"Tests"] = [[NSMutableArray alloc] init];
-        [self.userDataFile[@"Tests"] addObject:self.testData];
-    }
-}
-
--(void)writeUserDataToMemory {
-    if (!self.userDataFile || !self.userDataFilePath) {
-        [self getUserDataDocument];
-        
-        // If this block executes, something went wrong; raise exception
-        // [NSException raise:@"Invalid SpiroTest State Reached" format:@"Attempted to save data without user data file; no data destination"];
-    }
-    
-    // Create JSON encoded file from current user data (which may or may not be complete)
-    NSData* JSONFile = [self NSDataFromObject:self.userDataFile];
-    // Write updated user data to memory
-    [JSONFile writeToFile:self.userDataFilePath atomically:NO];
-}
-
--(NSMutableDictionary*)objectFromJSON: (NSData*) rawData {
-    return [NSJSONSerialization JSONObjectWithData:rawData options:NSJSONReadingMutableContainers error:nil];       // Return dictionary from JSON encoded NSData
-}
-
--(NSData*)NSDataFromObject:(NSDictionary*) dataObject {
-    return [NSJSONSerialization dataWithJSONObject:dataObject options:0 error:nil];                                 // Return JSON encoded NSData object from dictionary
 }
 
 
