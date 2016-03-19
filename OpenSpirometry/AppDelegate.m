17051eef3cf0ff3c631dbcba2fac20382df382c2
Spencer Kaiser <skaiser@me.com>
2016-03-19 14:32:31 -0500

    Adding implementation for modifying rotation (although it's commented out at the moment)

--- OpenSpirometry/AppDelegate.m
+++ OpenSpirometry/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+@import AVKit;
+@import AVFoundation;
 
 @interface AppDelegate ()
 
@@ -25,9 +27,23 @@
     pageControl.currentPageIndicatorTintColor = [UIColor blackColor];
     
     pageControl.backgroundColor = [UIColor whiteColor];
-
+    
     return YES;
 }
+
+//- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
+//{
+////    if ([self.window.rootViewController.presentedViewController isKindOfClass: [SecondViewController class]])
+////    {
+////        SecondViewController *secondController = (SecondViewController *) self.window.rootViewController.presentedViewController;
+////        
+////        if (secondController.isPresented)
+////            return UIInterfaceOrientationMaskAll;
+////        else return UIInterfaceOrientationMaskPortrait;
+////    }
+////    else
+//    return UIInterfaceOrientationMaskPortrait;
+//}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
